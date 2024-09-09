ebdcb2b263f752429b6cf78c58e9e608a889745a
Ian Calica <Iggc654@gmail.com>
2024-09-09 22:18:58 +0800

    updated

--- frontend/src/components/admin/Modal/Contact.jsx
+++ frontend/src/components/admin/Modal/Contact.jsx
@@ -4,10 +4,9 @@ import { getBorderColor } from "../../../Utils/borderColor";
 
 const Contact = ({
   onClose,
-  onCategoryCreated,
   notifySuccess,
   notifyError,
-  categoryToEdit,
+  contactToEdit,
   isEditing,
 }) => {
   const {
@@ -17,9 +16,94 @@ const Contact = ({
     formState: { errors },
   } = useForm();
 
+  // useEffect(() => {
+  //   if(isEditing && contactToEdit){
+  //     reset({})
+  //   }
+  // });
+  const onSubmit = async (data) => {};
   return (
-    <div>
-      <div></div>
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
+      <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-sm">
+        <h2 className="text-xl font-bold mb-4">
+          {isEditing ? "Edit Category" : "Add Category"}
+        </h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-4">
+            <label htmlFor="name" className="block text-gray-700 mb-2">
+              Name
+            </label>
+            <input
+              id="name"
+              type="text"
+              className={`w-full px-3 py-2 border rounded-md ${getBorderColor(
+                "name",
+                errors
+              )}`}
+              {...register("name", { required: "Category name is required" })}
+            />
+            {errors.name && (
+              <p className="text-red-500 text-sm mt-1">{errors.name.message}</p>
+            )}
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-700 mb-2">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              className={`w-full px-3 py-2 border rounded-md ${getBorderColor(
+                "email",
+                errors
+              )}`}
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: "Invalid email address",
+                },
+              })}
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm mt-1">{errors.name.message}</p>
+            )}
+          </div>
+          <div className="mb-4">
+            <label htmlFor="phone" className="block text-gray-700 mb-2">
+              Phone Number
+            </label>
+            <input
+              id="phone"
+              type="text"
+              maxLength={11}
+              className={`w-full px-3 py-2 border rounded-md ${getBorderColor(
+                "phone",
+                errors
+              )}`}
+              {...register("phone", { required: "Phone Number is required" })}
+            />
+            {errors.name && (
+              <p className="text-red-500 text-sm mt-1">{errors.name.message}</p>
+            )}
+          </div>
+          <div className="flex justify-end">
+            <button
+              type="button"
+              onClick={onClose}
+              className="px-4 py-2 rounded-md text-gray-500 border border-gray-300 mr-2"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="px-4 py-2 rounded-md font-semibold border-2 text-green-500 border-green-500 hover:bg-green-500 hover:text-white"
+            >
+              {isEditing ? "Update" : "Create"}
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
